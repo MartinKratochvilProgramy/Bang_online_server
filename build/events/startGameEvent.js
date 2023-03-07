b67df9eee54e63a6d0f58c44e8c3778cabbf1dcb
MartinKratochvilProgramy <martvil96@gmail.com>
2023-03-07 13:57:16 +0100

    normal deck

--- build/events/startGameEvent.js
+++ build/events/startGameEvent.js
@@ -4,11 +4,11 @@ exports.startGameEvent = void 0;
 var utils_1 = require("../utils");
 var server_1 = require("../server");
 var game_1 = require("../game");
-var deckDynamite_1 = require("../testDecks/deckDynamite");
+var deck_1 = require("../deck");
 var startGameEvent = function (io, data) {
     var roomName = data.currentRoom;
     try {
-        server_1.rooms[roomName].game = new game_1.Game(data.players, deckDynamite_1.deckDynamite);
+        server_1.rooms[roomName].game = new game_1.Game(data.players, deck_1.deck);
         console.log("Game started in room ", roomName, data.players);
         io.emit("rooms", (0, utils_1.getRoomsInfo)(server_1.rooms));
         io.to(roomName).emit("get_character_choices", server_1.rooms[roomName].game.genCharacterChoices());
