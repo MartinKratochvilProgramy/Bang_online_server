13d39fc4aede5fb13c31ffe55434cc35b45025dd
martin.kratochvil <martvil96@gmail.com>
2023-03-08 12:34:46 +0100

    fixed playBeer

--- events/playBeer.ts
+++ events/playBeer.ts
@@ -1,12 +1,22 @@
 import { rooms } from "../server";
-import { updateGameState } from "../utils/updateGameState";
+import { updateTopStackCard } from "../utils/updateTopStackCard";
 
 export const playBeer = (io: any, data: any) => {
     const roomName = data.currentRoom;
+    const username = data.username;
+
     if (rooms[roomName].game === null) return;
     try {
         io.to(roomName).emit("console", rooms[roomName].game!.useBeer(data.username, data.cardDigit, data.cardType));
-        updateGameState(io, roomName);
+        
+        const socketID = rooms[roomName].players.find(player => player.username === username)!.id;
+        io.to(socketID).emit("my_hand", rooms[roomName].game!.getPlayerHand(username));
+        io.to(roomName).emit("update_number_of_cards", {
+            username: username,
+            handSize: rooms[roomName].game!.getPlayerHand(username).length
+        })
+        updateTopStackCard(io, roomName);
+        
         io.to(roomName).emit("update_health", {
             username: data.username,
             health: rooms[roomName].game!.players[data.username].character.health
