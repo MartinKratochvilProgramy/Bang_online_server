02663fdf4f53b8185dc269199210e06ac3720203
martin.kratochvil <martvil96@gmail.com>
2023-03-08 11:49:42 +0100

    fixed drawFromDeck

--- events/drawFromDeck.ts
+++ events/drawFromDeck.ts
@@ -1,12 +1,19 @@
-import { updateGameState } from "../utils/updateGameState";
 import { rooms } from "../server";
 
 export const drawFromDeck = (io: any, data: any) => {
     const roomName = data.currentRoom;
+    const username = data.username;
     if (rooms[roomName].game === null) return;
     try {
         io.to(roomName).emit("console", rooms[roomName].game!.drawFromDeck(2, data.username));
-        updateGameState(io, roomName);
+
+        const socketID = rooms[roomName].players.find(player => player.username === username)!.id;
+        io.to(socketID).emit("my_hand", rooms[roomName].game!.getPlayerHand(username));
+        io.to(roomName).emit("update_number_of_cards", {
+            username: username,
+            handSize: rooms[roomName].game!.getPlayerHand(username).length
+        })
+
         io.to(roomName).emit("update_players_with_action_required", rooms[roomName].game!.getPlayersWithActionRequired());
     } catch (error) {
         console.log(`Error in room ${roomName}:`);
