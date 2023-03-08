a511a6f85a5cfe22d4e7a3639d0ade0bd22335bc
martin.kratochvil <martvil96@gmail.com>
2023-03-08 12:50:24 +0100

    woring duel, draw cards

--- events/playDuel.ts
+++ events/playDuel.ts
@@ -1,14 +1,31 @@
 import { rooms } from "../server";
-import { updateGameState } from "../utils/updateGameState";
+import { updateTopStackCard } from "../utils/updateTopStackCard";
 
 export const playDuel = (io: any, data: any) => {
     const roomName = data.currentRoom;
+    const username = data.username;
+    
     if (rooms[roomName].game === null) return;
     try {
         io.to(roomName).emit("console", rooms[roomName].game!.useDuel(data.target, data.cardDigit, data.cardType));
         io.to(roomName).emit("duel_active", rooms[roomName].game!.duelActive);
         io.to(roomName).emit("update_players_losing_health", rooms[roomName].game!.getPlayersLosingHealth());
-        updateGameState(io, roomName);
+        
+        const player1 = rooms[roomName].game!.duelPlayers[0];
+        const player2 = rooms[roomName].game!.duelPlayers[1];
+        
+        const socketID1 = rooms[roomName].players.find(player => player.username === player1)!.id;
+        const socketID2 = rooms[roomName].players.find(player => player.username === player2)!.id;
+
+        io.to(socketID1).emit("my_hand", rooms[roomName].game!.getPlayerHand(player1));
+        io.to(socketID2).emit("my_hand", rooms[roomName].game!.getPlayerHand(player2));
+        
+        io.to(roomName).emit("update_number_of_cards", {
+            username: username,
+            handSize: rooms[roomName].game!.getPlayerHand(username).length
+        })
+        
+        updateTopStackCard(io, roomName);
     } catch (error) {
         console.log(`Error in room ${roomName}:`);
         console.log(error);
