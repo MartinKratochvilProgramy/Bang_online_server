f6f2b5bbd0cc4988807f744fbaec503941f1c721
martin.kratochvil <martvil96@gmail.com>
2023-03-08 11:57:45 +0100

    fixed getEmporioCard

--- events/getEmporioCard.ts
+++ events/getEmporioCard.ts
@@ -3,12 +3,20 @@ import { updateGameState } from "../utils/updateGameState";
 
 export const getEmporioCard = (io: any, data: any) => {
     const roomName = data.currentRoom;
+    const username = data.username;
+
     if (rooms[roomName].game === null) return;
     try {
         rooms[roomName].game!.getEmporioCard(data.username, data.card);
         // send emporio state to clients
         io.to(roomName).emit("emporio_state", { cards: rooms[roomName].game!.emporio, nextEmporioTurn: rooms[roomName].game!.nextEmporioTurn });
-        updateGameState(io, roomName);
+        
+        const socketID = rooms[roomName].players.find(player => player.username === username)!.id;
+        io.to(socketID).emit("my_hand", rooms[roomName].game!.getPlayerHand(username));
+        io.to(roomName).emit("update_number_of_cards", {
+            username: username,
+            handSize: rooms[roomName].game!.getPlayerHand(username).length
+        })
     } catch (error) {
         console.log(`Error in room ${roomName}:`);
         console.log(error);
