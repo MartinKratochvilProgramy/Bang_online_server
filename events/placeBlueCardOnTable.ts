3a7ae2912a0e4abcd3646c331544c49b93910ef0
martin.kratochvil <martvil96@gmail.com>
2023-03-08 12:09:23 +0100

    fixed placeBlueCardOnTable

--- events/placeBlueCardOnTable.ts
+++ events/placeBlueCardOnTable.ts
@@ -1,12 +1,21 @@
 import { rooms } from "../server";
-import { updateGameState } from "../utils/updateGameState";
+import { updatePlayerTables } from "../utils/updatePlayerTables";
 
 export const placeBlueCardOnTable = (io: any, data: any) => {
     const roomName = data.currentRoom;
+    const username = data.username;
+    
     if (rooms[roomName].game === null) return;
     try {
         io.to(roomName).emit("console", rooms[roomName].game!.placeBlueCardOnTable(data.card));
-        updateGameState(io, roomName);
+        
+        const socketID = rooms[roomName].players.find(player => player.username === username)!.id;
+        io.to(socketID).emit("my_hand", rooms[roomName].game!.getPlayerHand(username));
+        io.to(roomName).emit("update_number_of_cards", {
+            username: username,
+            handSize: rooms[roomName].game!.getPlayerHand(username).length
+        })
+        updatePlayerTables(io, roomName);
     } catch (error) {
         console.log(`Error in room ${roomName}:`);
         console.log(error);
