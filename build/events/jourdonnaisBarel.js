5614d6ca97b4228a7b0b266eaf3fd06e83ec08c3
MartinKratochvilProgramy <martvil96@gmail.com>
2023-03-09 10:19:49 +0100

    update Jourdonnais hand on barel

--- build/events/jourdonnaisBarel.js
+++ build/events/jourdonnaisBarel.js
@@ -5,12 +5,15 @@ var server_1 = require("../server");
 var updatePlayerTables_1 = require("../utils/updatePlayerTables");
 var jourdonnaisBarel = function (io, data) {
     var roomName = data.currentRoom;
+    var username = data.username;
     if (server_1.rooms[roomName].game === null)
         return;
     try {
         io.to(roomName).emit("console", server_1.rooms[roomName].game.jourdonnaisBarel(data.username));
         io.to(roomName).emit("update_players_losing_health", server_1.rooms[roomName].game.getPlayersLosingHealth());
         (0, updatePlayerTables_1.updatePlayerTables)(io, roomName);
+        var socketID = server_1.rooms[roomName].players.find(function (player) { return player.username === username; }).id;
+        io.to(socketID).emit("my_hand", server_1.rooms[roomName].game.getPlayerHand(username));
     }
     catch (error) {
         console.log("Error in room ".concat(roomName, ":"));
--- events/jourdonnaisBarel.ts
+++ events/jourdonnaisBarel.ts
@@ -3,11 +3,18 @@ import { updatePlayerTables } from "../utils/updatePlayerTables";
 
 export const jourdonnaisBarel = (io: any, data: any) => {
     const roomName = data.currentRoom;
+    const username = data.username;
+
     if (rooms[roomName].game === null) return;
+
     try {
         io.to(roomName).emit("console", rooms[roomName].game!.jourdonnaisBarel(data.username));
         io.to(roomName).emit("update_players_losing_health", rooms[roomName].game!.getPlayersLosingHealth());
+
         updatePlayerTables(io, roomName);
+
+        const socketID = rooms[roomName].players.find(player => player.username === username)!.id;
+        io.to(socketID).emit("my_hand", rooms[roomName].game!.getPlayerHand(username))
     } catch (error) {
         console.log(`Error in room ${roomName}:`);
         console.log(error);
